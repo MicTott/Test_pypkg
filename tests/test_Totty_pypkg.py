c5db58aac341a11ce6eb9976f7c8a9363912693f
Michael Totty <mictott@gmail.com>
2021-08-23 15:54:40 -0500

    Test test

--- tests/test_Totty_pypkg.py
+++ tests/test_Totty_pypkg.py
@@ -1,1 +1,8 @@
 from Totty_pypkg import Totty_pypkg
+
+def test_catbind():
+    a = pd.Categorical(["character", "hits", "your", "eyeballs"])
+    b = pd.Categorical(["but", "integer", "where it", "counts"])
+    assert ((Totty_pypkg.catbind(a, b)).codes == [1, 4, 7, 3, 0, 5, 6, 2]).all()
+    assert ((Totty_pypkg.catbind(a, b)).categories == ["but", "character",
+            "counts", "eyeballs", "hits", "integer", "where it", "your"]).all()
